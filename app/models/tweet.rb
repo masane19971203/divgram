884043b5c678b230e647bdf83c25c1d8031fd8c4
masane19971203 <m03biglove-mq@docomo.ne.jp>
2020-06-11 20:59:22 +0900

    create model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
-  # belongs_to :user
-  # has_many :comments
-  # has_many :tweets_hashtags
-  # has_many :hashtags, through: :tweets_hashtags
+  belongs_to :user
+  has_many :comments
+  has_many :tweets_hashtags
+  has_many :hashtags, through: :tweets_hashtags
 end
--- app/models/tweet_hashtag.rb
+++ app/models/tweet_hashtag.rb
@@ -1,4 +1,4 @@
 class TweetHashtag < ApplicationRecord
-  # belongs_to :tweet
-  # belongs_to :hashtag
+  belongs_to :tweet
+  belongs_to :hashtag
 end
